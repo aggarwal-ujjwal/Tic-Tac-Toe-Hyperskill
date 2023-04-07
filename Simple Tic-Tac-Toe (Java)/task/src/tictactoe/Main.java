7f5bdbeea6afd8778afb42dfe1a0922c53e9120f
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-04-07 21:14:36 +0530

    Stage 4 solution

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -6,19 +6,76 @@ import java.util.Scanner;
 import static java.lang.Math.abs;
 
 public class Main {
+
     public static void main(String[] args) {
         // write your code here
         //Tic-Tac-Toe
+         HashMap<Character, Integer> charCountMap
+                = new HashMap<Character, Integer>();
         String input = userInput();
         printGrid(input);
-        gameState(input);
+        userMakeMove(input, charCountMap);
+       // gameState(input);
     }
 
-    private static void gameState(String input) {
+    private static void userMakeMove(String input, HashMap<Character, Integer> charCountMap) {
+        Scanner scanner = new Scanner(System.in);
+            int a = 0,b =0;
+//        System.out.println("x = " + x);
+//        System.out.println("y = " + y);
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                String line = scanner.nextLine();
+                //Imp line
+                //int num = Integer.parseInt(line.replaceAll("[\\D]", ""));
+                String[] parts = line.split(" ");
+                a = Integer.parseInt(parts[0]);
+                b = Integer.parseInt(parts[1]);
+                if((a>3 || a<=0) || b>3 || b<=0) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    continue;
+                }
+                int index = decideIndex(a,b);
+                if(input.charAt(index)=='X' || input.charAt(index)=='O') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                    continue;
 
-        HashMap<Character, Integer> charCountMap
-                = new HashMap<Character, Integer>();
+                }
+                validInput = true;
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+//                scanner.nextLine(); // clear the invalid input from the scanner buffer
+            }
+        }
 
+
+        int index = decideIndex(a,b); // index of the character to be changed
+        String numOfOYString = numOfOX(input, charCountMap);
+        String[] parts = numOfOYString.split(" ");
+        int numOfO = parts[0]==null?0:Integer.parseInt(parts[0]);
+        int numOfX = parts[1]==null?0:Integer.parseInt(parts[1]);
+        char newChar;
+        if(numOfX>numOfO)
+            newChar = 'O';
+        else
+            newChar = 'X';
+        String newStr = input.substring(0, index) + newChar + input.substring(index+1);
+        printGrid(newStr);
+    }
+
+    public static int decideIndex(int a, int b) {
+        int index = 0;
+        if(a==1){
+            index = b-1;
+        } else if (a==2) {
+            index = b+2 ;
+        } else {
+            index = b+5;
+        }
+        return index;
+    }
+    public static String numOfOX(String input, HashMap<Character, Integer> charCountMap){
         char[] strArray = input.toCharArray();
 
         for (char c : strArray) {
@@ -30,9 +87,18 @@ public class Main {
             }
         }
 
-        int numOfX = charCountMap.get('X') ;
-        int numOfO = charCountMap.get('O');
-        
+        Integer numOfX = charCountMap.size()==1?0:charCountMap.get('X') ;
+        Integer numOfO = charCountMap.size()==1?0:charCountMap.get('O') ;
+        String result = numOfO + " " + numOfX;
+        return result;
+    }
+    private static void gameState(String input, HashMap<Character, Integer> charCountMap ) {
+
+        String numOfOYString = numOfOX(input,charCountMap);
+        String[] parts = numOfOYString.split(" ");
+        int numOfO = Integer.parseInt(parts[0]);
+        int numOfX = Integer.parseInt(parts[1]);
+
         boolean xWin = false;
         boolean oWin = false;
         //if X wins
