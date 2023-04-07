2d3346ada4d598b11875095c1f18c63a322a0606
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-04-07 18:02:20 +0530

    Stage 2 solution

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -1,11 +1,29 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
         //Tic-Tac-Toe
-        System.out.println("X O X\n" +
-                "O X O\n" +
-                "X X O ");
+        String input = userInput();
+        printGrid(input);
+    }
+
+    private static void printGrid(String input) {
+        System.out.println("---------");
+        System.out.printf("| %c %c %c |\n" +
+                           "| %c %c %c |\n" +
+                           "| %c %c %c |\n",
+                            input.charAt(0),input.charAt(1),input.charAt(2),
+                            input.charAt(3),input.charAt(4),input.charAt(5),
+                            input.charAt(6),input.charAt(7),input.charAt(8));
+        System.out.println("---------");
+    }
+
+    public static String userInput(){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
     }
 }
